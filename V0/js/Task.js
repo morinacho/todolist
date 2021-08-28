f4591c01797afe5337e09e4b04dde4a22b5b2f2a
Ignacio Morinigo <morinacho@gmail.com>
2021-08-28 00:38:15 -0300

    Version 0

---
+++ V0/js/Task.js
@@ -0,0 +1,28 @@
+const createTask = (task) => {
+    const container = document.querySelector(".container");
+    const Task = ` <section class="task">
+                        <input type="checkbox">
+                        <span class="completed">${task}</span>
+                        <a href="javascript:void(0)" class="material-icons" onclick="deleteTask(event);">delete</a>
+                    </section> `;
+    container.insertAdjacentHTML("beforeend", Task);
+}
+const deleteTask = (event) => {
+    let container = document.querySelector(".container");
+    container.removeChild(event.target.parentElement);
+} 
+
+document.querySelector("#add-task").addEventListener("click", (event) => {
+        event.preventDefault(); 
+        const task = document.querySelector("#task-name");
+
+        if(task.value != ""){ 
+            createTask(task.value)
+            task.value = ""; 
+        }
+    }
+);
+
+function deleteTask(getTask){
+    getTask.closest("li").remove();
+}
