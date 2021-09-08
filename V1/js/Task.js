fccd5cb18b7804f5703aa0be0e2901d74b5590dd
Ignacio Morinigo <morinacho@gmail.com>
2021-09-08 07:18:02 -0300

    Version 1

--- V1/js/Task.js
+++ V1/js/Task.js
@@ -4,14 +4,11 @@ const createTask = (task) => {
                         <input type="checkbox">
                         <span class="completed">${task}</span>
                         <a href="javascript:void(0)" class="material-icons" onclick="deleteTask(event);">delete</a>
+                        <a href="javascript:void(0)" class="material-icons" onclick="shareTask(event);">share</a>
+                        <a href="javascript:void(0)" class="material-icons" onclick="copyText(event);">content_paste</a>
                     </section> `;
     container.insertAdjacentHTML("afterend", Task);
 }
-const deleteTask = (event) => {
-    let container = document.querySelector(".container");
-    container.removeChild(event.target.parentElement);
-} 
-
 document.querySelector("#add-task").addEventListener("click", (event) => {
         event.preventDefault(); 
         const task = document.querySelector("#task-name");
@@ -22,3 +19,45 @@ document.querySelector("#add-task").addEventListener("click", (event) => {
         }
     }
 );
+
+// Eliminar una tarea
+const deleteTask = (event) => {
+    let container = document.querySelector(".container");
+    container.removeChild(event.target.parentElement);
+} 
+
+/*
+ * Change v.1 
+ */
+// Poner en full screen
+document.querySelector("#fullscreem").addEventListener("click", (event) =>{
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+        event.target.textContent = "fullscreen_exit";
+    } else if (document.exitFullscreen) {
+        document.exitFullscreen(); 
+        event.target.textContent = "fullscreen"; 
+    }
+});
+
+// Compartir tarea
+function shareTask(event){
+    if (!("share" in navigator)) {
+        alert('Web Share API not supported.');
+    return;
+      }
+    
+    navigator.share({
+        title: 'Check mi tarea',
+        text: this.copyText(event),
+        url: window.location
+    })
+    .then(() => console.log('Successful share'))
+    .catch(error => console.log('Error sharing:', error));
+}
+
+// Copiar en cache el contenido de una tarea en cache
+function copyText(event){
+    var taskText = event.target.parentElement.children[1].textContent
+    navigator.clipboard.writeText(taskText);  
+}
